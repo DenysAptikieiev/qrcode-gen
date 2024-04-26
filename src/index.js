db7da5b65c3808e9f921500712f4a7263fba6f8b
DenysFront <denys.aptikieiev@alphanavtech.com>
2024-04-26 16:41:30 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -5,8 +5,4 @@ import Root from "./routes/Root";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-      <Root/>
-  </React.StrictMode>
-);
+root.render(<Root/>);
